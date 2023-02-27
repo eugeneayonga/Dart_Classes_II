95b761a1fa71d89c50db97033eb9551da456245a
Eugene <eugeneayonga@gmail.com>
2023-02-27 12:41:08 +0300

    Initial commit

---
+++ bin/dart_classes_two.dart
@@ -0,0 +1,5 @@
+import 'package:dart_classes_two/dart_classes_two.dart' as dart_classes_two;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_classes_two.calculate()}!');
+}
---
+++ test/dart_classes_two_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_classes_two/dart_classes_two.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
