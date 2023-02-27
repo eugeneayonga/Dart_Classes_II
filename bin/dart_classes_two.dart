d985c04bdabfee251cc72111d9ad5a795c7380ee
Eugene <eugeneayonga@gmail.com>
2023-02-27 13:43:56 +0300

    Code refactor

--- bin/dart_classes_two.dart
+++ bin/dart_classes_two.dart
@@ -1,5 +1,11 @@
 import 'package:dart_classes_two/dart_classes_two.dart' as dart_classes_two;
+import 'package:dart_classes_two/dart_class_inheritance.dart'
+    as dart_class_inheritance;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_classes_two.calculate()}!');
+  print('***********************************************');
+
+  print('${dart_class_inheritance.animalsDisplay()}');
+  print('***********************************************');
 }
