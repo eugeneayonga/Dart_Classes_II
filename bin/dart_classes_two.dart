ffc1b7f9fba489ebf6e7d1b37ddce09ca0159600
Eugene <eugeneayonga@gmail.com>
2023-02-27 16:02:48 +0300

    Imported abstraction package

--- bin/dart_classes_two.dart
+++ bin/dart_classes_two.dart
@@ -1,11 +1,16 @@
 import 'package:dart_classes_two/dart_classes_two.dart' as dart_classes_two;
 import 'package:dart_classes_two/dart_class_inheritance.dart'
     as dart_class_inheritance;
+import 'package:dart_classes_two/dart_class_abstraction.dart'
+    as dart_class_abstraction;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_classes_two.calculate()}!');
   print('***********************************************');
 
   print('${dart_class_inheritance.animalsDisplay()}');
+  print('***********************************************');
+
+  print('${dart_class_abstraction.abstractionDisplay()}');
   print('***********************************************');
 }
